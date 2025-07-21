72dd57985d1770b5a12a211c1821f531d6d9232d
Arcky97 <syntixis@gmail.com>
2025-07-21 13:57:38 +0200

    website release v1.0.8.1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center bg-gray-900 text-white">
       <h1 className="text-3xl font-bold">About Us</h1>
-      <p>v1.0.7</p>
+      <p>v1.0.8</p>
     </main>
   );
 }
