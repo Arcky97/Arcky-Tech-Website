771623e399d2ec73c8b5944067899aa043b20087
Arcky97 <syntixis@gmail.com>
2025-08-25 18:30:34 +0200

    website release v1.14.2.1

--- app/doggo-bot/dashboard/[guildId]/embeds/page.tsx
+++ app/doggo-bot/dashboard/[guildId]/embeds/page.tsx
@@ -1,28 +1,28 @@
 "use client"
-import fetchTableData from "@/lib/db/fetchTableData";
-import { useQuery } from "@tanstack/react-query"
-import { useParams } from "next/navigation"
+//import fetchTableData from "@/lib/db/fetchTableData";
+//import { useQuery } from "@tanstack/react-query"
+//import { useParams } from "next/navigation"
 
 export default function Embeds() {
 
-  const { guildId } = useParams<{ guildId: string }>();
-  const { data: genEmbeds, isLoading: genEmbedsLoading } = useQuery({
+  //const { guildId } = useParams<{ guildId: string }>();
+  /*const { data: genEmbeds, isLoading: genEmbedsLoading } = useQuery({
     queryKey: ["generatedEmbeds", guildId],
     queryFn: () => fetchTableData("GeneratedEmbeds", guildId),
     staleTime: 1000 * 60 * 5,
     enabled: !!guildId,
     refetchOnMount: false,
     refetchOnWindowFocus: false
-  });
+  });*/
 
-  const { data: eventEmbeds, isLoading: eventEmbedsLoading } = useQuery({
+  /*const { data: eventEmbeds, isLoading: eventEmbedsLoading } = useQuery({
     queryKey: ["eventEmbeds", guildId],
     queryFn: () => fetchTableData("EventEmbeds", guildId),
     staleTime: 1000 * 60 * 50,
     enabled: !!guildId,
     refetchOnMount: false,
     refetchOnWindowFocus: false
-  })
+  })*/
 
   return (
     <div>
