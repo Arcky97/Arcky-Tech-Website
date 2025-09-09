768229007b7f7d2c5c08bb9e16edd9f01b0818a8
Arcky97 <syntixis@gmail.com>
2025-09-09 21:36:08 +0200

    website release v1.15.6.2

--- lib/db/dataRemovers/removeTableData.ts
+++ lib/db/dataRemovers/removeTableData.ts
@@ -2,7 +2,7 @@ import { createFetchRemoveInit } from "../createFetchRequestInit";
 import { createCatchErrorMessage, createResErrorMessage, createTableErrorMessage } from "../createErrorMessage";
 import getEndPointUrl from "../getEndPointUrl";
 
-export const deleteGeneratedEmbed = async (tableName: string, guildId: string, embedId: number) => {
+export const deleteGeneratedEmbed = async (tableName: string, guildId: string, embedId: number | null) => {
   try {
     if (!tableName) throw new Error(createTableErrorMessage());
 
