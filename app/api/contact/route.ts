07462684847a01b415ea231da18765a4e19d0f02
Arcky97 <syntixis@gmail.com>
2026-01-11 21:14:50 +0100

    website release v1.20.0.6

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-if (!process.env.RESEND_API_KEY) {
-  throw new Error("RESEND_API_KEY is not set");
-}
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
+
+  if (!process.env.RESEND_API_KEY) {
+    throw new Error("RESEND_API_KEY is not set");
+  }
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   try {
     const formData = await req.formData();
 
