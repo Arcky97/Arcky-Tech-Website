f3823cfce2f6faa75429b91110d8c12cc03521a9
Arcky97 <syntixis@gmail.com>
2026-02-12 13:15:30 +0100

    website release v1.21.3.0

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 
 export default function ReloadOnActionError() {
   useEffect(() => {
+    console.log("ReloadOnActionError, useEffect")
     const originalFetch = window.fetch;
 
     window.fetch = async (...args) => {
@@ -11,6 +12,7 @@ export default function ReloadOnActionError() {
 
       if (res.status === 500) {
         const text = await res.clone().text();
+        console.log("ReloadOnActionError, text", text);
         if (text.includes("Failed to find Server Action")) {
           window.location.reload();
         }
